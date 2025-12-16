4fe9dff3f8a7a297de7fd34c2d9e50eb8fed5e82
Krzysztof Augustyniak <augustyniak@o2.pl>
2025-12-16 12:39:26 +0100

    Contact

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ function hamburger() {
   } else {
     x.className = "topnav";
   }
-} 
+}
 //zmienne tablice
 const contactPeople = [
-  { name: "Karol",
+  {
+    name: "Karol",
     email: "karol@email.com",
     message: "Hello, I've reviewed your impressive portfolio and am interested in discussing a potential collaboration. Please call me at 712-218-123 to talk further."
   },
-  { name: "Ernest",
+  {
+    name: "Ernest",
     email: "ernest@email.com",
     message: "Hello, Please call me at 351-152-555 to talk further."
   },
-  { name: "Jan",
+  {
+    name: "Jan",
     email: "jan@email.com",
     message: "Welcome Jan. You created really nice project",
   }
@@ -33,7 +36,6 @@ const homeLink = document.querySelectorAll(".homeLink");
 const nameClass = document.querySelector(".name");
 const jobClass = document.querySelector(".job");
 const main = document.querySelector("main");
-
 
 //dodanie event listenerów
 messagesLink.forEach(link => link.addEventListener("click", messagesPage));
@@ -66,12 +68,125 @@ function messagesPage() {
   });
 }
 
+function contactPage() {
+  activClassOff();
+  clearMain();
+
+  contactLink.forEach(link => link.classList.add("active"));
+  nameClass.textContent = "CONTACT ME";
+  jobClass.textContent = "SAY HELLO TO ME";
+
+  main.innerHTML = `
+  <form class="contactPageView">
+    <div class="contactTitle">Contact me</div>
+
+    <div class="contactRow">
+      <div class="field">
+        <label for="Name">Name</label>
+        <input type="text" id="Name" placeholder="Your Name">
+        <span class="error-message" id="nameError"></span>
+      </div>
+      <div class="field">
+        <label for="Email">Email</label>
+        <input type="email" id="Email" placeholder="email@example.com">
+        <span class="error-message" id="emailError"></span>
+      </div>
+    </div>
+
+    <div class="field">
+      <label for="Message">Message</label>
+      <textarea id="Message" placeholder="Hello, my name is ..."></textarea>
+      <span class="error-message" id="messageError"></span>
+    </div>
+
+    <button id="contactButton" type="submit">Send message</button>
+  </form>
+`;
+  const form = document.querySelector('form');
+  const nameInput = document.getElementById('Name');
+  const emailInput = document.getElementById('Email');
+  const messageInput = document.getElementById('Message');
+  const nameError = document.getElementById('nameError');
+  const emailError = document.getElementById('emailError');
+  const messageError = document.getElementById('messageError');
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let isValid = true;
+
+    nameError.textContent = '';
+    emailError.textContent = '';
+    messageError.textContent = '';
+    if (nameInput.value.trim().length < 3) {
+      nameError.textContent = "The name must be at least 3 characters long.";
+      document.getElementById('Name').style.borderBottom = '2px solid #AF0808';
+      isValid = false;
+    } else if (nameInput.value.trim().length > 20) {
+      nameError.textContent = "The name must not exceed 20 characters." 
+      document.getElementById('Name').style.borderBottom = '2px solid #AF0808';
+      isValid = false;
+    } else {
+      nameError.textContent = '';
+      document.getElementById('Name').style.borderBottom = '2px solid #1F2041';
+    }
+    if (!emailPattern.test(emailInput.value)) {
+      emailError.textContent = "Please enter a valid email address.";
+      document.getElementById('Email').style.borderBottom = '2px solid #AF0808';
+      isValid = false;
+    } else {
+      emailError.textContent = '';
+      document.getElementById('Email').style.borderBottom = '2px solid #1F2041';
+    }
+    if (messageInput.value.trim() === '') {
+      messageError.textContent = "The message cannot be empty.";
+      document.getElementById('Message').style.borderBottom = '2px solid #AF0808';
+      isValid = false;
+    } else if (messageInput.value.trim().length > 100) {
+      messageError.textContent = "The message must not exceed 100 characters.";
+      document.getElementById('Message').style.borderBottom = '2px solid #AF0808';
+      isValid = false;
+    } else {
+      messageError.textContent = '';
+      document.getElementById('Message').style.borderBottom = '2px solid #1F2041'
+    }
+    if (isValid) {
+      contactPeople.push({
+        name: nameInput.value.trim(),
+        email: emailInput.value.trim(),
+        message: messageInput.value,
+      });
+      alert('Message sent successfully!');
+      messagesPage();
+    }
+
+  });
+}
+
+function projectsPage() {
+  activClassOff();
+  clearMain();
+  projectsLink.forEach(link => link.classList.add("active"));
+}
+function aboutPage() {
+  activClassOff();
+  clearMain();
+  aboutLink.forEach(link => link.classList.add("active"));
+}
+function homePage() {
+  activClassOff();
+  clearMain();
+  homeLink.forEach(link => link.classList.add("active"));
+}
 
 function activClassOff() {
   const links = document.querySelectorAll(".topnav a");
   links.forEach((link) => {
     link.classList.remove("active");
   });
+  const linksFooter = document.querySelectorAll(".footer-left a");
+  linksFooter.forEach((link) => {
+    link.classList.remove("active");
+  })
 }
 function clearMain() {
   main.innerHTML = "";
